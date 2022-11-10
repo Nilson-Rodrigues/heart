783e687570f38f7607e9bf85b385298ca06b0e59
Francinilson Rodrigues <58186025+Nilson-Rodrigues@users.noreply.github.com>
2022-11-10 15:53:24 -0300

    fist-commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function troca () {
+    document.getElementById("imgtroca").innerHTML = "<img src='../Heart/img/niver.gif'>"
+    document.getElementById("texttroca").innerHTML = "<p>Riririr</p>"
+}
